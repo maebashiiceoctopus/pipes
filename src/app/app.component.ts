1e54ba82cb742341f11d7fd11074986135840f9b
Alejandra <macostaq94@gmail.com>
2020-12-28 09:40:41 -0500

    Pipes en angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,43 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pipes';
+  nombre: string = 'Azeroth';
+  nombre2: string='AlPAca sEsI';
+  activar: boolean= true;
+
+  personajes: string[] = [
+    'Malfurion',
+    'Illidan',
+    'Tyrande',
+    'Arthas',
+    'Garrosh',
+    'Thrall',
+  ];
+
+  PI: number = Math.PI;
+
+  porcentaje: number = 0.234;
+
+  salario: number = 1234.5;
+
+  videoUrl:string ='https://www.youtube.com/embed/anbI0kfi_rs';
+  horda = {
+    nombre: 'Thrall',
+    clave: 'Horda',
+    direccion: {
+      calle: 'Azeroth',
+      casa: 'ogrimar',
+    },
+  };
+
+  valorPromesa = new Promise <string>((resolve) => {
+    setTimeout(() => {
+      resolve('llego la data');
+    }, 4500);
+  });
+
+  fecha: Date = new Date();
 }
